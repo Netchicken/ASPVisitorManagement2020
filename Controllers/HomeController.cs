c851276264317cb0a1972f31e7fc129cd03ca838
Gary Dix <gary.d@atcltd.atc.org.nz>
2020-08-17 10:02:12 +1200

    Text File Operations

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ASPVisitorManagement2020.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ASPVisitorManagement2020.Models;
+using System.Diagnostics;
+using System.IO;
 
 namespace ASPVisitorManagement2020.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+
+
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
+
+            ViewBag.Welcome = "Welcome to the Visitor Management System V2";
+
+            ViewBag.NewVisitor = new Visitor()
+            {
+
+                FirstName = "Howard",
+                LastName = "Davis"
+
+            };
+
+            ViewData["Welcome2"] = "This is another welcome";
+
+
+            //===============COA==============
+
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            FileInfo filePath = new FileInfo(Path.Combine(webRootPath, "COA.txt"));
+            string[] lines = System.IO.File.ReadAllLines(filePath.ToString());
+            ViewData["Conditions"] = lines;
+
+
+
+
             return View();
         }
 
@@ -33,5 +62,9 @@ namespace ASPVisitorManagement2020.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+
+    public interface IWebHostingEnvironment
+    {
     }
 }
